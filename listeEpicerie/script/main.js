3694e3869fa150bbb065947a84e300fb96b7c120
MI7QC <54377301+MI7QC@users.noreply.github.com>
2020-06-07 11:43:30 -0400

    Add files via upload

--- listeEpicerie/script/main.js
+++ listeEpicerie/script/main.js
@@ -4,19 +4,16 @@ console.log(jQuery.fn.jquery);
 
 // input bouton
 let input = $("input").css("fontSize", "20px");
-console.log(input);
 
 // recuperation du Ul via sont id liste
 let liste = $("#liste").css({ "fontSize": "30px", "list-style-type": "none" });
-console.log(liste);
-
 
 //fonction insertion de text dans la liste.
 input.on("keydown", function (evt) {
     if (evt.which == 13) { // Code ASCII de la touche Entrée
 
         // creation d'un li et text en lowerCase
-        liste.append('<li><a>' + input.val().toLowerCase() + '</a></li>').last();
+        liste.append('<li id="liste-item"><a>' + input.val() + '</a></li>').last();
 
         // array qui sert a insérer les text doublon
         let listForRemove = [];
@@ -34,38 +31,54 @@ input.on("keydown", function (evt) {
 });
 
 
-//fonction qui est pas terminé  modification d'un li (text a)
+//fonction modifier un element li
 $(document).ready(function () {
     $('#liste').on("click", "li", function () {
-        $(this).css({ "border-style": "solid", "border-color": "coral" });
-        console.log($(this).text());
 
+        //recuperation de la valeur du champ text input.
+        let input = $('<input value="' + $(this).text() + '"/>');
+
+        //remplace  this qui est un li en input
+        $(this).replaceWith(input);
+        //quand on click sur l'element le css est appliqué
+        $(input).css({ "background-color": "darkcyan", "fontSize": "30px" })
+
+        // si je clique sur un autre element celui deja cliquer revient a la normal
+        $(input).focus();
+
+        // permet de declancher cette evenement sur un element
+        $(input).blur(function () {
+            $(this).replaceWith('<li id="liste-item"><a>' + input.val() + '</a></li>');
+        })
+
+        //fonction modification du text dans la liste.
+        input.on("keydown", function (evt) {
+            if (evt.which == 13) {
+                // array qui sert a insérer les text doublon
+                let listForRemove = [];
+
+                // replace this qui est un input en li
+                $(this).replaceWith('<li id="liste-item"><a>' + input.val() + '</a></li>');
+
+                // function qui boucle tous les li et verifie si le champ text inséré est deja present dans la liste, si oui il est supprimé
+                $('#liste li').each(function () {
+                    if (jQuery.inArray(this.innerHTML, listForRemove) == -1) {
+                        listForRemove.push(this.innerHTML);
+                    }
+                    else {
+                        $(this).remove();
+                    }
+                });
+            }
+        })
     });
 });
-
-
-
 
 // fonction qui permet de suprimer un element li a l'aide de la touche CTRL + click gauche
 $('#liste').on("click", "li", function (e) {
     if (e.ctrlKey)
-        $(this).css({ "border-style": "solid", "border-color": "coral" }).remove();
-
+        $(this).remove();
 });
-
-
-
-
-
-
-
-
-// sert a insérer la valeur dans le champ text, va etre utile quand on va clicker sur un li
-// let zone_saisie = $("input[name='zone_saisie']").val("testest");
-// console.log(zone_saisie);
-// liste.children("li").filter(function (i, elem) {
-//     return $(elem).text().toLowerCase() === input.text().trim().toLowerCase();
-// }.length === 0);
 
 
 
